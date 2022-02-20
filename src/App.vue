132e6f8ea55f0283fc9e1f751d3fc92736ee42da
pauljeffreys1974 <35877133+pauljeffreys1974@users.noreply.github.com>
2022-02-20 14:32:49 -0500

    Splitting the two arrays to prioritise Wordle words and addingfooter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <WordleSolve msg="Welcome to Your Vue.js App"/>
+  <WordleSolve />
+  <Footer />
 </template>
 
 <script>
 import WordleSolve from './components/WordleSolve.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    WordleSolve
+    WordleSolve,
+    Footer
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 10px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="footer">
+        &copy;2022 Paul Jeffreys | <a href="mailto:pjcavey74@googlemail.com">Contact Me</a>
+    </div>
+</template>
+
+<script>
+    export default { name: 'Footer' }
+</script>
+
+<style scoped>
+    .footer {
+        background-color: darkslategrey;
+        padding: 40px 20px;
+        text-align: center;
+        color: #fff;
+        margin-top: 20px;
+    }
+
+    a {
+        color: #fff;
+    }
+</style>
